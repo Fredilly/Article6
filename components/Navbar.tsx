377352a861736ba3c91256c5fc478472d7c9b442
fredilly <fredilly@yahoo.com>
2023-11-11 11:28:09 +0800

    Added slide down to mobile menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { Bars2Icon, XMarkIcon } from '@heroicons/react/24/outline'; // Import the XMarkIcon for the close button
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { navigationLinks } from '../utils/navigation';
 
 const Navbar: React.FC = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   const renderLinks = (mobile = false) => (
     navigationLinks.map(link => (
       <Link key={link.href} href={link.href} legacyBehavior>
-        <a className={`hover:text-green-600 transition-colors duration-300 ease-in-out ${mobile ? 'border-b border-white py-8 text-2xl text-left px-8 w-full' : ''}`}>{link.label}</a>
+        <a 
+          className={`hover:text-green-600 transition-colors duration-300 ease-in-out ${mobile ? 'border-b border-white py-8 text-2xl text-left px-8 w-full' : ''}`}
+          onClick={mobile ? toggleMobileMenu : undefined}
+        >
+          {link.label}
+        </a>
       </Link>
     ))
   );
@@ -29,20 +36,20 @@ const Navbar: React.FC = () => {
         {/* Mobile Navigation - Hamburger Icon */}
         <div className="md:hidden">
           <button onClick={toggleMobileMenu}>
-            <Bars2Icon className="h-6 w-6" />
+            <Bars3Icon className="h-6 w-6" />
           </button>
         </div>
       </div>
 
       {/* Mobile Navigation - Menu Overlay */}
-      {isMobileMenuOpen && (
-        <div className="md:hidden bg-green-100 w-full h-full absolute top-0 left-0 flex flex-col justify-start pt-20 z-50">
-          <button onClick={toggleMobileMenu} className="absolute top-4 right-4">
-            <XMarkIcon className="h-6 w-6" />
-          </button>
-          {renderLinks(true)} {/* Mobile-specific styles applied here */}
-        </div>
-      )}
+      <div 
+        className={`md:hidden bg-green-100 w-full h-full absolute top-0 left-0 flex flex-col justify-start pt-20 z-50 transition-transform duration-300 ease-in-out ${isMobileMenuOpen ? 'translate-y-0' : '-translate-y-full'}`}
+      >
+        <button onClick={toggleMobileMenu} className="absolute top-4 right-4">
+          <XMarkIcon className="h-6 w-6" />
+        </button>
+        {renderLinks(true)}
+      </div>
     </nav>
   );
 };
