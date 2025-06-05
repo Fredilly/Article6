efadf9d000bf76c655708f10d6a0e50a2db64480
fredilly <fredilly@yahoo.com>
2025-06-05 22:15:51 +0100

    changed the footer to  Article6

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-200 text-center p-4 text-sm text-gray-700">
-      <p>&copy; {new Date().getFullYear()} Articlesix All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Article6 All rights reserved.</p>
       {/* You can add more footer content here such as social links */}
     </footer>
   );
