766c34a6a1488d6c5c29e394a56b8caecff5d05c
Fred E <fredilly@yahoo.com>
2025-08-08 01:57:21 +0800

    feat: use updated 480p hero video

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,7 +5,7 @@ const HeroSection: React.FC = () => {
     <div className="relative w-full h-screen overflow-hidden">
       <video
         className="absolute inset-0 w-full h-full object-cover"
-        src="https://ik.imagekit.io/tzublgy5d/Article6/hero360.mp4?updatedAt=1754587990038"
+        src="https://ik.imagekit.io/tzublgy5d/Article6/hero480.mp4?updatedAt=1754588076486"
         autoPlay
         loop
         muted
