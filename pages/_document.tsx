2bb748db089c36c77854159796e64dc891252e83
Fredilly <fredilly@article6.org>
2026-07-28 15:19:35 +0800

    feat: add green logo and favicon matching app.article6.org design
    - Add reusable Logo component: A6 mark in green rounded square
      (bg-forest-700) + Article6 wordmark (text-forest-700)
    - Replace text-only brand in NavBar, PreviewHeader, PreviewFooter
      with Logo component
    - Update Footer brand color from green-600 to forest-700
    - Add green favicon.svg (A6 in white on forest-700 background,
      same layout as app.article6.org but green instead of black)
    - Add favicon link to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,8 @@ class MyDocument extends Document {
       <Html lang={this.props.__NEXT_DATA__.locale ?? 'en'}>
         {/* Set the document language; default to English but allow dynamic locale if provided */}
         <Head>
-          {/* Other tags like your stylesheet links can also go here */}
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="16x16" />
         </Head>
         <body>
           <Main />
