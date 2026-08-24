49108d0eaf1c0b8abe82448b4df9f3ac41ff497a
Fred E <fredilly@article6.org>
2026-08-24 21:36:58 +0800

    Give parent homepage independent chrome

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,6 @@
 import React, { ReactNode } from 'react';
 import { useRouter } from 'next/router';
 import NavBar from './NavBar';
-import PreviewHeader from './preview/PreviewHeader';
 import Footer from './Footer';
 
 interface LayoutProps {
@@ -13,10 +12,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const router = useRouter();
   const isHome = router.pathname === '/';
 
+  if (isHome) {
+    return <>{children}</>;
+  }
+
   return (
-    <div className="flex flex-col min-h-screen">
-      {isHome ? <PreviewHeader /> : <NavBar />}
-      <main className={`flex-grow ${isHome ? '' : 'pt-16'}`}>{children}</main>
+    <div className="flex min-h-screen flex-col">
+      <NavBar />
+      <main className="flex-grow pt-16">{children}</main>
       <Footer />
     </div>
   );
