727d831edddda209e9e7cd8624d47c0dd1d4dd3e
Fred E <fredilly@article6.org>
2026-09-06 16:19:37 +0800

    Remove legacy global navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 // components/Layout.tsx
 import React, { ReactNode } from 'react';
 import { useRouter } from 'next/router';
-import NavBar from './NavBar';
 import Footer from './Footer';
 
 interface LayoutProps {
@@ -10,16 +9,15 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const router = useRouter();
-  const isParentSurface = router.pathname === '/' || router.pathname === '/contact';
+  const isParentSurface = router.pathname === '/' || router.pathname === '/contact' || router.pathname === '/approach';
 
   if (isParentSurface) {
     return <>{children}</>;
   }
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <NavBar />
-      <main className="flex-grow pt-16">{children}</main>
+    <div className="flex min-h-screen flex-col">
+      <main className="flex-grow">{children}</main>
       <Footer />
     </div>
   );
